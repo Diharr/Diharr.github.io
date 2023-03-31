76717b4e254c27321c9d9035ac5ea6f74b1235e0
Dihsar <ackrashid@gmail.com>
2023-03-31 16:31:51 -0400

    changed background picture and some other tweaks

--- script.js
+++ script.js
@@ -7,5 +7,17 @@ hamburger.addEventListener('click', e=>{
     headerBar.classList.toggle('active');
 });
 
-
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "ackrashid@gmail.com",
+        Password : "Enderdude13#",
+        To : 'ackrashid@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "Website email",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
 
